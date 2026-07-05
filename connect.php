9a9c9fb81eb5589e9c7686de71edafab9811bdb3
fiqri <hhih6153@gmail.com>
2026-07-05 18:02:09 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$pass = "";
-$dbname = "sistem_jadwal_kuliah"; //nama data base
-$conn = new mysqli($servername, $username, $pass, $dbname);
-if ($conn->connect_error) {
-    echo "Koneksi Gagal ".$conn->connect_error;
-}
-?>
+
